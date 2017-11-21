a73742cbf969943f18565d2b8ef45ef377696c73
chekholin <kennville@gmail.com>
2017-11-21 21:56:05 +0300

    create canvas

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hi!");
+
+let canvas = document.createElement("canvas");
+
+Object.assign(canvas, { className: "canvas scene", width: 800, height: 400 });
+
+console.log(canvas);
+
+
+document.body.appendChild(canvas);
