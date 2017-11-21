74fad5f31b94d1434354ff6f8b1b8bb4eab839fe
chekholin <kennville@gmail.com>
2017-11-21 23:02:44 +0300

    create button

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+const width = 800;
+const height = 400;
 
 let canvas = document.createElement("canvas");
+let button = document.createElement("button");
 
-Object.assign(canvas, { className: "canvas scene", width: 800, height: 400 });
-
-console.log(canvas);
-
+Object.assign(canvas, { className: "canvas scene", width, height });
+Object.assign(button, { className: "button start-button"});
 
 document.body.appendChild(canvas);
+document.body.appendChild(button);
+
+
