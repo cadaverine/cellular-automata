23f8b2a7173cf542b1b6c72f23c998b2475cc546
chekholin <kennville@gmail.com>
2017-11-21 23:17:22 +0300

    update main.js

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const width = 800;
 const height = 400;
 
 let canvas = document.createElement("canvas");
-let button = document.createElement("button");
+let startButton = document.createElement("button");
 
 Object.assign(canvas, { className: "canvas scene", width, height });
-Object.assign(button, { className: "button start-button"});
+Object.assign(startButton, { className: "button start-button"});
 
+let startButtonText = document.createTextNode("Start");
+
+startButton.appendChild(startButtonText);
 document.body.appendChild(canvas);
-document.body.appendChild(button);
+document.body.appendChild(startButton);
+
 
 
