5f6198afdf6ed93110d423fd311d7cd2c47315ee
chekholin <kennville@gmail.com>
2017-11-22 08:05:42 +0300

    add stop button

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ const height = 400;
 
 let canvas = document.createElement("canvas");
 let startButton = document.createElement("button");
+let stopButton = document.createElement("button");
 
 Object.assign(canvas, { className: "canvas scene", width, height });
 Object.assign(startButton, { className: "button start-button"});
+Object.assign(stopButton, { className: "button stop-button"});
 
 let startButtonText = document.createTextNode("Start");
+let stopButtonText = document.createTextNode("Stop");
 
 startButton.appendChild(startButtonText);
+stopButton.appendChild(stopButtonText);
+
 document.body.appendChild(canvas);
 document.body.appendChild(startButton);
+document.body.appendChild(stopButton);
 
 
+let ctx = canvas.getContext("2d");
 
+
+console.log(ctx);
