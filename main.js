af484721e301ac4c634585f9db423d37ca245baf
chekholin <kennville@gmail.com>
2017-11-24 20:21:53 +0300

    add range

--- main.js
+++ main.js
@@ -11,12 +11,22 @@ let stopButton = document.createElement("button");
 let clearButton = document.createElement("button");
 let startButtonText = document.createTextNode("Start");
 let stopButtonText = document.createTextNode("Stop");
-let clearButtonText= document.createTextNode("Clear");
+let clearButtonText = document.createTextNode("Clear");
+
+let colorRange = document.createElement("input")
 
 Object.assign(canvas, { className: "canvas scene", width, height });
 Object.assign(startButton, { className: "button start-button"});
 Object.assign(stopButton, { className: "button stop-button"});
 Object.assign(clearButton, { className: "button clear-button"});
+Object.assign(colorRange, {
+	className: "range color-range",
+	type: "range",
+	min: "0",
+	max: "255",
+	step: "10",
+	value: "255"
+});
 
 Object.assign(mainContainer, { className: "container main-container" });
 Object.assign(buttonContainer, { className: "container button-container" });
@@ -27,6 +37,7 @@ clearButton.appendChild(clearButtonText);
 buttonContainer.appendChild(startButton);
 buttonContainer.appendChild(stopButton);
 buttonContainer.appendChild(clearButton);
+buttonContainer.appendChild(colorRange);
 
 mainContainer.appendChild(canvas);
 mainContainer.appendChild(buttonContainer);
@@ -39,6 +50,7 @@ let ctx = canvas.getContext("2d");
 let output = ctx.createImageData(canvas.width, canvas.height);
 let length = output.data.length;
 
+let range = colorRange.value;
 
 let interval = null;
 
@@ -46,7 +58,7 @@ startButton.onclick = () => {
 	if (!interval) {
 		interval = setInterval(() => {
 			for (var i = 0; i < length; i++) {
-				output.data[i] = Math.round(255 * Math.random());
+				output.data[i] = Math.round(255 - range * Math.random());
 			}
 			ctx.putImageData(output, 0, 0);
 		}, 60);
@@ -67,6 +79,10 @@ clearButton.onclick = () => {
 		output.data[i] = 255;
 	}
 	ctx.putImageData(output, 0, 0);
+}
+
+colorRange.oninput = () => {
+	range = colorRange.value;
 }
 
 
